b37f42fba323f1644faecb847833a94992916e62
EthanDenyer <e.denyer@hotmail.co.uk>
2023-09-13 13:10:20 +0100

    commit edit

--- experimental.py
+++ experimental.py
@@ -1,6 +1,59 @@
-variable = "5"
-print(type(variable))
-print(variable.replace(variable, "**"+variable+"**"))
+# TITLE = input(str("[1] What would you like the Heading of your markdown file to be called?   "))
+# TITLE_SIZE = input(str("[2] What size (level 1-6) would you like your Heading to be?   "))    # STRING INPUT  |  #Title Size is determined by hashtags before the title 
+#     #Add in a requirement that TITLE_SIZE must be numerical or else we will display an error message and make them repeat the question. 
 
-new_int = print(float(variable))
-print(type(new_int))
+# #TITLESIZE EDITS:
+# if TITLE_SIZE == "1":
+#     TITLE_SIZE_1 = TITLE.replace(TITLE, "# "+TITLE)
+# if TITLE_SIZE == "2":
+#     TITLE_SIZE_2 = TITLE.replace(TITLE, "## "+TITLE)
+# if TITLE_SIZE == "3":
+#     TITLE_SIZE_3 = TITLE.replace(TITLE, "### "+TITLE)
+# if TITLE_SIZE == "4":
+#     TITLE_SIZE_4 = TITLE.replace(TITLE, "#### "+TITLE)
+# if TITLE_SIZE == "5":
+#     TITLE_SIZE_5 = TITLE.replace(TITLE, "##### "+TITLE)
+# if TITLE_SIZE == "6":
+#     TITLE_SIZE_6 = TITLE.replace(TITLE, "###### "+TITLE)
+
+# if TITLE_SIZE_1 > "0":
+#     TT_FORWARD = TITLE_SIZE_1
+# if TITLE_SIZE_2 > "0":
+#     TT_FORWARD = TITLE_SIZE_2
+# if TITLE_SIZE_3 > "0":
+#     TT_FORWARD = TITLE_SIZE_3
+# if TITLE_SIZE_4 > "0":
+#     TT_FORWARD = TITLE_SIZE_4
+# if TITLE_SIZE_5 > "0":
+#     TT_FORWARD = TITLE_SIZE_5
+# if TITLE_SIZE_6 > "0":
+#     TT_FORWARD = TITLE_SIZE_6
+# print(TT_FORWARD)
+
+# input_ 
+# def 
+#I want to pass the title size to the function and have that function assign it to an overall variable which print's one of them .
+
+# new_input = input("Give me a number")
+
+# def user_input(input):
+#     if input == "0":
+#         print("hello")
+#     else:
+#         print("goodbye")
+
+# user_input(new_input)     
+N123_SUB_HEADERS = input("[6] Please list all the different subheaders that are required. Seperated by a space.   ") # Because we have more than one subheader we now need to seperate them each by 
+NEW_HEADER_list = N123_SUB_HEADERS.split() #Example HEADER list = ["Functions Progression Specifications Resources Limits Computing"]
+List_Value = NEW_HEADER_list
+
+# HEAD_0 = NEW_HEADER_list[0]
+# HEAD_1 = NEW_HEADER_list[1]
+# HEAD_2 = NEW_HEADER_list[2]
+# HEAD_3 = NEW_HEADER_list[3]
+# HEAD_4 = NEW_HEADER_list[4]
+# HEAD_5 = NEW_HEADER_list[5]
+# HEAD_6 = NEW_HEADER_list[6]
+# HEAD_7 = NEW_HEADER_list[7]
+# HEAD_8 = NEW_HEADER_list[8]
+# HEAD_9 = NEW_HEADER_list[9]
--- source_code.py
+++ source_code.py
@@ -3,6 +3,7 @@
 # 1.02
 
 import time
+from time import sleep
 
 
 print("Please answer all questions directly, unless indicated otherwise by: (Y/N) ")
@@ -10,20 +11,22 @@ print("Please answer all questions directly, unless indicated otherwise by: (Y/N
 
 TITLE = input(str("[1] What would you like the Heading of your markdown file to be called?   "))
 TITLE_SIZE = input(str("[2] What size (level 1-6) would you like your Heading to be?   "))    # STRING INPUT  |  #Title Size is determined by hashtags before the title 
+    #Add in a requirement that TITLE_SIZE must be numerical or else we will display an error message and make them repeat the question. 
 
 #TITLESIZE EDITS:
 if TITLE_SIZE == "1":
-    TITLE_SIZE_1 = TITLE.replace(TITLE, "# "+TITLE)
+    TITLE_SIZE_VARIABLE = TITLE.replace(TITLE, "# "+TITLE)
 if TITLE_SIZE == "2":
-    TITLE_SIZE_2 = TITLE.replace(TITLE, "## "+TITLE)
+    TITLE_SIZE_VARIABLE = TITLE.replace(TITLE, "## "+TITLE)
 if TITLE_SIZE == "3":
-    TITLE_SIZE_3 = TITLE.replace(TITLE, "### "+TITLE)
+    TITLE_SIZE_VARIABLE = TITLE.replace(TITLE, "### "+TITLE)
 if TITLE_SIZE == "4":
-    TITLE_SIZE_4 = TITLE.replace(TITLE, "#### "+TITLE)
+    TITLE_SIZE_VARIABLE = TITLE.replace(TITLE, "#### "+TITLE)
 if TITLE_SIZE == "5":
-    TITLE_SIZE_5 = TITLE.replace(TITLE, "##### "+TITLE)
+    TITLE_SIZE_VARIABLE = TITLE.replace(TITLE, "##### "+TITLE)
 if TITLE_SIZE == "6":
-    TITLE_SIZE_6 = TITLE.replace(TITLE, "###### "+TITLE)
+    TITLE_SIZE_VARIABLE = TITLE.replace(TITLE, "###### "+TITLE)
+
 DESCRIPTION = input(str("[3] Please give a brief description on the contents of your file:   "))
 DESCRIPTION_EDITS = input(str("[4] Is there any part of your description [3] that you would like to edit? E.g make a word bold (Y/N)   "))
 
@@ -50,13 +53,42 @@ if DESCRIPTION_EDITS == "Y":
         NEW_ITALICS = DESCRIPTION_EDITS_CHANGES.replace(DESCRIPTION_EDITS_CHANGES, "*"+DESCRIPTION_EDITS_CHANGES+"*")
         # print(NEW_ITALICS)
 else:
-    SUB_HEADERS = input(str("[5] How many subheaders are required within the document?")) # STRING INPUT 
+    SUB_HEADERS = input(str("[5] How many subheaders are required within the document? (Maximum of ten)   ")) # STRING INPUT 
 
 #END OF DESCRIPTION EDITS
 
-#ADDITIONAL HEADERS:
-# if SUB_HEADERS == 
-#END OF DESCRIPTION EDITS
+# ADDITIONAL HEADERS:
+
+if SUB_HEADERS == "0":
+   sleep(0.5)
+   print("Is this working")
+if SUB_HEADERS == "1":
+    N1_SUB_HEADERS = input("[6] Please list all the different subheaders that are required. Seperated by a space.   ")
+if SUB_HEADERS > "1":
+    N123_SUB_HEADERS = input("[6] Please list all the different subheaders that are required. Seperated by a space.   ") # Because we have more than one subheader we now need to seperate them each by 
+    NEW_HEADER_list = N123_SUB_HEADERS.split() #Example HEADER list = ["Functions Progression Specifications Resources Limits Computing"]
+    List_Value = len(NEW_HEADER_list)    #We want to assign the value of the elements in our list, this will then be used in our function 
+
+if SUB_HEADERS < "0":
+    print("Invalid Input")
 
 
+HEAD_0 = NEW_HEADER_list[0]
+HEAD_1 = NEW_HEADER_list[1]
+HEAD_2 = NEW_HEADER_list[2]
+HEAD_3 = NEW_HEADER_list[3]
+HEAD_4 = NEW_HEADER_list[4]
+HEAD_5 = NEW_HEADER_list[5]
+HEAD_6 = NEW_HEADER_list[6]
+HEAD_7 = NEW_HEADER_list[7]
+HEAD_8 = NEW_HEADER_list[8]
+HEAD_9 = NEW_HEADER_list[9]
+ #NOTE TO MYSELF I WILL NEED TO CHANGE THE SUB HEADER LATER 
+#END OF ADDITIONAL HEADERS
+
+# OUTPUTTED RESULT:
+print("------------------------------------")
+print("---------GENERATED .MD CODE---------")
+print(TITLE_SIZE_VARIABLE)
+# END OUTPUTTED RESULT:
 
