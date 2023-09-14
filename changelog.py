f2b20dffc9c11adf184d162908691fac126eb057
e.denyer@hotmail.co.uk <e.denyer@hotmail.co.uk>
2023-09-14 15:18:32 +0100

    commit edit V.1.03

---
+++ changelog.py
@@ -0,0 +1,15 @@
+#CHANGELOG 
+#CURRENT APPLICATION VERSION = V.1.03
+
+#SYSTEMS:
+#TITLESIZE EDIT = OPERATIONAL | OUTPUTTING
+#DESCRIPTION EDIT = OPERATIONAL | OUTPUTTING
+#ADDITIONAL HEADERS = NON-OPERATIONAL | NON-OUTPUTTING
+#
+#-----------------------
+# V.1.03 
+# Working code, outputs format including amended title size and edited description.
+# Added functionality for code to print unedited description if user requests.
+# Addition of System Messages to provide user input feedback.
+##-----------------------
+
