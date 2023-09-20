3dedaedbcb76614e870a9fa2ee0137fee96782e4
e.denyer@hotmail.co.uk <e.denyer@hotmail.co.uk>
2023-09-20 17:35:53 +0100

    experimental changes

--- experimental.py
+++ experimental.py
@@ -1,3 +1,4 @@
+
 # TITLE = input(str("[1] What would you like the Heading of your markdown file to be called?   "))
 # TITLE_SIZE = input(str("[2] What size (level 1-6) would you like your Heading to be?   "))    # STRING INPUT  |  #Title Size is determined by hashtags before the title 
 #     #Add in a requirement that TITLE_SIZE must be numerical or else we will display an error message and make them repeat the question. 
@@ -43,24 +44,34 @@
 #         print("goodbye")
 
 # user_input(new_input)     
+SUB_HEADERS = input("[5] How many subheaders are required within the document? (Maximum of ten)   ") #String input
+SUB_HEADER_INT = int(SUB_HEADERS)
 N123_SUB_HEADERS = input("[6] Please list all the different subheaders that are required. Seperated by a space.   ") # Because we have more than one subheader we now need to seperate them each by 
-NEW_HEADER_list = N123_SUB_HEADERS.split() #Example HEADER list = ["Functions Progression Specifications Resources Limits Computing"]
+NEW_HEADER_list = N123_SUB_HEADERS.split() #We take the input and we give each individual word a set of brackets
 List_Value = len(NEW_HEADER_list) #Number of strings in the list (INTEGER)
 
 
+
+print(NEW_HEADER_list) # IF WE PAY ATTENTION WE SEE THAT THE [ ] DESIGNATED THAT THIS IS ALREADY IN ARRAY FORM. 
+print(SUB_HEADERS)
+print(type(SUB_HEADERS))
 #13/9/23: FUNC ATTEMPT 1 - I WANT TO CREATE A WORKING INPUT WHICH ALLOWS US TO ASSIGN A VARIABLE NAME ITERATION ON NUMBER OF CHANGES
-List_Value_Dic = {}
 
-def assignindex(input):
-    input = List_Value #This has appended six values 
-    print(List_Value)  
-    for i in range(List_Value)      #This will now print the amount of values in the range
-    #I need a function that allows us to create the number of variables which are in List_Value
+def array_var():
+    for x in range(SUB_HEADER_INT):     # We are now cycling through the number of values 
+        
 
-#int isn't iterable, so we need a way
+array_var()
 
-print(type(List_Value),"LIST VALUE")   #This confirms that List_Value is an integer. We cannot iterate a Integer
-assignindex(input)
+# def assignindex(input):
+#     input = List_Value #This has appended six values 
+#     print(List_Value)  
+#     for i in range(List_Value)      #This will now print the amount of values in the range
+#     #I need a function that allows us to create the number of variables which are in List_Value
+
+# #int isn't iterable, so we need a way
+
+# print
 # assignindex(N123_SUB_HEADERS)
 # HEAD_0 = NEW_HEADER_list[0]
 # HEAD_1 = NEW_HEADER_list[1]
@@ -80,5 +91,3 @@ assignindex(input)
 
 # print(type(dog))
 
-# for i in range(dog):
-#     print("Hello")    #SO WHAT
